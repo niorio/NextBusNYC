f91dd7e239147d666b90346ab0ecd06b172a614a
Nicholas Iorio <niorio1@gmail.com>
2015-05-03 20:40:43 -0400

    changed max number of stops

--- app/controllers/api/stops_controller.rb
+++ app/controllers/api/stops_controller.rb
@@ -2,7 +2,7 @@ class Api::StopsController < ApplicationController
 
   def nearby
     here = Geokit::LatLng.new(params[:lat], params[:long])
-    @stops = Stop.by_distance({ origin: here }).limit(10)
+    @stops = Stop.by_distance({ origin: here }).limit(12)
     render json: @stops
   end
 
